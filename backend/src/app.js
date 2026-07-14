534004a941e025687f4300088905d481f33a49cc
Premendu Mondal <mondalpremendu0@gmail.com>
2026-07-15 03:10:01 +0530

    setup morgan

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
 
 
 const app = express();
 
+
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use((req, res) => {
+    res.status(404).json({ message: "OOPS!! PAGE NOT FOUND" });
+});
 
 module.exports = app;
